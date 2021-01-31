de93d8f9e998278e474f3bdc0357006456761d74
wwz1999 <1853928@tongji.edu.cn>
2021-01-31 23:55:53 +0800

    Signed-off-by: wwz1999 <1853928@tongji.edu.cn>

--- group20.cc
+++ group20.cc
@@ -13,7 +13,7 @@ using namespace ns3;
 
 int main(int argc, char *argv[])
 {
-
+//define related params
   //ue number
   uint16_t numberOfUes = 1;
   //enb number
@@ -22,8 +22,6 @@ int main(int argc, char *argv[])
   double interPacketInterval = 100;
   //length of time
   double simTime = 10;
-  //not konw yet
-  double enbTxPowerDbm = 46.0;
   //moving speed of ue
   double movingSpeedOfUes = 500;
 
@@ -38,6 +36,13 @@ int main(int argc, char *argv[])
   //attri of A2A4 algorithm
   uint16_t neighbourCellOffset = 1;
 
+  //tx power of the enbs
+  //all enb have same tx power
+  double enbTxPowerDbm = 46.0;
+  //noiseFigure of the enbs
+  double noiseFigure = 5.0;
+  //macToChannelDelay of enbs
+  uint16_t macToChannelDelay = 2;
   // Command line arguments
   CommandLine cmd;
   cmd.AddValue("simTime", "Total duration of the simulation /seconds", simTime);
@@ -49,11 +54,24 @@ int main(int argc, char *argv[])
   cmd.AddValue("timeToTrigger", "timeToTrigger of A3 algorithm", timeToTrigger);
   cmd.AddValue("neighbourCellOffset", "neighbourCellOffset of A2A4 algorithm", neighbourCellOffset);
   cmd.AddValue("servingCellThreshold", "servingCellThreshold of A2A4 algorithm", servingCellThreshold);
-  // cmd.AddValue("", "", );
-  // cmd.AddValue("", "", );
+  cmd.AddValue("noiseFigure", "Loss (dB) in the Signal-to-Noise-Ratio due to  non-idealities in the receiver", noiseFigure);
+  cmd.AddValue("macToChannelDelay", "The delay in TTI units that occurs between   a scheduling decision in the MAC and the actual  start of the transmission by the PHY", macToChannelDelay);
+
   // cmd.AddValue("", "", );
   // cmd.AddValue("", "", );
   cmd.Parse(argc, argv);
+  //any other attributes needed can be config like above
+  //and make it work like below 
+
+//attributes and their description and default value can be found in 
+//   /src/lte/model/a2-a4-rsrq-handover-algorithm.cc
+//   /src/lte/model/a3-rsrq-handover-algorithm.cc
+//  /src/lte/model/lte-enb-phy.cc
+//  /src/lte/model/lte-enb-mac.cc
+//  /src/lte/model/lte-ue-phy.cc
+//  /src/lte/model/lte-ue-mac.cc
+//  /src/lte/model/lte-ue-power-control.cc
+//and so on 
 
   // change some default attributes
   Config::SetDefault("ns3::UdpClient::Interval", TimeValue(MilliSeconds(10)));
@@ -61,6 +79,8 @@ int main(int argc, char *argv[])
   Config::SetDefault("ns3::LteHelper::UseIdealRrc", BooleanValue(true));
   //change some physic attribute about lte
   Config::SetDefault("ns3::LteEnbPhy::TxPower", DoubleValue(enbTxPowerDbm));
+  Config::SetDefault("ns3::LteEnbPhy::NoiseFigure", DoubleValue(noiseFigure));
+  Config::SetDefault("ns3::LteEnbPhy:: MacToChannelDelay", UintegerValue(macToChannelDelay));
   //and so on
 
   Ptr<LteHelper> lteHelper = CreateObject<LteHelper>();
@@ -77,7 +97,6 @@ int main(int argc, char *argv[])
                                            TimeValue(MilliSeconds(timeToTrigger)));
   if (handoverAlgo == "A2A4")
   {
-
     lteHelper->SetHandoverAlgorithmType("ns3::A2A4RsrqHandoverAlgorithm");
     lteHelper->SetHandoverAlgorithmAttribute("ServingCellThreshold",
                                              UintegerValue(servingCellThreshold));
@@ -94,24 +113,6 @@ int main(int argc, char *argv[])
   InternetStackHelper internet;
   internet.Install(remoteHostContainer);
 
-  // Create the Internet
-  PointToPointHelper p2ph;
-  p2ph.SetDeviceAttribute("DataRate", DataRateValue(DataRate("100Gb/s")));
-  p2ph.SetDeviceAttribute("Mtu", UintegerValue(1500));
-  p2ph.SetChannelAttribute("Delay", TimeValue(Seconds(0.010)));
-
-  NetDeviceContainer internetDevices = p2ph.Install(pgw, remoteHost);
-  Ipv4AddressHelper ipv4h;
-  ipv4h.SetBase("1.0.0.0", "255.0.0.0");
-  Ipv4InterfaceContainer internetIpIfaces = ipv4h.Assign(internetDevices);
-  Ipv4Address remoteHostAddr = internetIpIfaces.GetAddress(1);
-
-  // Routing of the Internet Host (towards the LTE network)
-  Ipv4StaticRoutingHelper ipv4RoutingHelper;
-  Ptr<Ipv4StaticRouting> remoteHostStaticRouting = ipv4RoutingHelper.GetStaticRouting(remoteHost->GetObject<Ipv4>());
-  // interface 0 is localhost, 1 is the p2p device
-  remoteHostStaticRouting->AddNetworkRouteTo(Ipv4Address("7.0.0.0"), Ipv4Mask("255.0.0.0"), 1);
-
   //create ue and enb nodes
   NodeContainer ueNodes;
   NodeContainer enbNodes;
@@ -120,7 +121,8 @@ int main(int argc, char *argv[])
 
   //lym:dynamicly adjust moving range
   int sqrtResult = (int)sqrt((double)numberOfEnbs);
-  if(sqrtResult * sqrtResult != numberOfEnbs) sqrtResult = sqrtResult + 1;
+  if (sqrtResult * sqrtResult != numberOfEnbs)
+    sqrtResult = sqrtResult + 1;
   double xRange = (double)sqrtResult * 500;
   double yRange = (double)sqrtResult * 500;
 
@@ -129,14 +131,14 @@ int main(int argc, char *argv[])
   MobilityHelper enbMobility;
   enbMobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
   enbMobility.SetPositionAllocator("ns3::GridPositionAllocator",
-                                "LayoutType", StringValue("RowFirst"), 
-                                "MinX", DoubleValue(0.0), 
-                                "MinY", DoubleValue(0.0), 
-                                "DeltaX", DoubleValue(500),
-                                "DeltaY", DoubleValue(500),
-                                "GridWidth", UintegerValue(sqrtResult));
+                                   "LayoutType", StringValue("RowFirst"),
+                                   "MinX", DoubleValue(0.0),
+                                   "MinY", DoubleValue(0.0),
+                                   "DeltaX", DoubleValue(500),
+                                   "DeltaY", DoubleValue(500),
+                                   "GridWidth", UintegerValue(sqrtResult));
   enbMobility.Install(enbNodes);
-  
+
   //set position of pgw
   MobilityHelper pgwMobility;
   pgwMobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
@@ -148,30 +150,30 @@ int main(int argc, char *argv[])
   MobilityHelper remoteHostMobility;
   remoteHostMobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
   remoteHostMobility.SetPositionAllocator("ns3::GridPositionAllocator", "MinX", DoubleValue(1400.0), "MinY",
-                                   DoubleValue(1300.0));
+                                          DoubleValue(1300.0));
   remoteHostMobility.Install(remoteHostContainer);
 
-  //make ue move 
+  //make ue move
   //lym:random position initialize
   MobilityHelper ueMobility;
 
-  ueMobility.SetPositionAllocator ("ns3::RandomDiscPositionAllocator",
-                             "X", StringValue ("500.0"),
-                             "Y", StringValue ("500.0"),
-                             "Rho", StringValue ("ns3::UniformRandomVariable[Min=200.0|Max=500.0]"),
-                             "Theta", StringValue ("ns3::UniformRandomVariable[Min=0.0|Max=6.2830]"));
+  ueMobility.SetPositionAllocator("ns3::RandomDiscPositionAllocator",
+                                  "X", StringValue("500.0"),
+                                  "Y", StringValue("500.0"),
+                                  "Rho", StringValue("ns3::UniformRandomVariable[Min=200.0|Max=500.0]"),
+                                  "Theta", StringValue("ns3::UniformRandomVariable[Min=0.0|Max=6.2830]"));
   // ueMobility.SetMobilityModel("ns3::WaypointMobilityModel");
   //lym:random move model
   //lym:modifiable moving speed
   char speedStr[100];
   sprintf(speedStr, "ns3::UniformRandomVariable[Min=%f|Max=%f]", movingSpeedOfUes, movingSpeedOfUes);
-  ueMobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
-                         "Mode", EnumValue (RandomWalk2dMobilityModel::MODE_TIME),
-                        //  "Distance", DoubleValue (300.0),
-                         "Time", TimeValue (Seconds (6.0)),
-                         "Direction", StringValue ("ns3::UniformRandomVariable[Min=0.0|Max=30.28]"),
-                         "Speed", StringValue (speedStr),
-                         "Bounds", RectangleValue (Rectangle (0.0, xRange, 0.0, yRange)));
+  ueMobility.SetMobilityModel("ns3::RandomWalk2dMobilityModel",
+                              "Mode", EnumValue(RandomWalk2dMobilityModel::MODE_TIME),
+                              //  "Distance", DoubleValue (300.0),
+                              "Time", TimeValue(Seconds(6.0)),
+                              "Direction", StringValue("ns3::UniformRandomVariable[Min=0.0|Max=30.28]"),
+                              "Speed", StringValue(speedStr),
+                              "Bounds", RectangleValue(Rectangle(0.0, xRange, 0.0, yRange)));
   ueMobility.Install(ueNodes);
 
   // Install LTE Devices in eNB and UEs
@@ -184,6 +186,27 @@ int main(int argc, char *argv[])
   Ipv4InterfaceContainer ueIpIfaces;
   ueIpIfaces = epcHelper->AssignUeIpv4Address(NetDeviceContainer(ueLteDevs));
 
+  // Create the Internet
+  PointToPointHelper p2ph;
+  p2ph.SetDeviceAttribute("DataRate", DataRateValue(DataRate("100Gb/s")));
+  p2ph.SetDeviceAttribute("Mtu", UintegerValue(1500));
+  p2ph.SetChannelAttribute("Delay", TimeValue(Seconds(0.010)));
+
+  //let ue connect with remotehost
+  //!not sure  link ue and remotehost or link pgw and remotehost  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  // NetDeviceContainer internetDevices = p2ph.Install(pgw, remoteHost);
+  NetDeviceContainer internetDevices = p2ph.Install(ueNodes.Get(0), remoteHost);
+  Ipv4AddressHelper ipv4h;
+  ipv4h.SetBase("1.0.0.0", "255.0.0.0");
+  Ipv4InterfaceContainer internetIpIfaces = ipv4h.Assign(internetDevices);
+  Ipv4Address remoteHostAddr = internetIpIfaces.GetAddress(1);
+
+  // Routing of the Internet Host (towards the LTE network)
+  Ipv4StaticRoutingHelper ipv4RoutingHelper;
+  Ptr<Ipv4StaticRouting> remoteHostStaticRouting = ipv4RoutingHelper.GetStaticRouting(remoteHost->GetObject<Ipv4>());
+  // interface 0 is localhost, 1 is the p2p device
+  remoteHostStaticRouting->AddNetworkRouteTo(Ipv4Address("7.0.0.0"), Ipv4Mask("255.0.0.0"), 1);
+
   // Set the default gateway for the UE
   Ptr<Node> ue = ueNodes.Get(0);
   Ptr<Ipv4StaticRouting> ueStaticRouting = ipv4RoutingHelper.GetStaticRouting(ue->GetObject<Ipv4>());
@@ -191,7 +214,6 @@ int main(int argc, char *argv[])
 
   // Attach UE to the first eNodeB
   lteHelper->Attach(ueLteDevs.Get(0), enbLteDevs.Get(0));
-
   // Install and start applications on UE and remote host
   uint16_t dlPort = 10000;
   uint16_t ulPort = 20000;
@@ -246,8 +268,9 @@ int main(int argc, char *argv[])
   lteHelper->EnableRlcTraces();
   lteHelper->EnablePdcpTraces();
 
-  //enable  pcap  traces
-  p2ph.EnablePcapAll("group20");
+  //enable  pcap  traces save the data in /IPdata
+  //should create the folder first
+  p2ph.EnablePcapAll("IPdata/group20");
 
   //not known yet
   Ptr<RadioBearerStatsCalculator> rlcStats = lteHelper->GetRlcStats();
@@ -258,12 +281,12 @@ int main(int argc, char *argv[])
   //animation
   //set the xml file name
   AnimationInterface anim("group20.xml");
-  //img url
-  //absolute path !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ 
   //Tower img resource
   uint32_t resourceTower;
   //Phone imig resource
   uint32_t resourcePhone;
+   //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!   img url should use absolute path   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   resourceTower = anim.AddResource("/home/matrix/ns3/ns-allinone-3.28/ns-3.28/examples/tutorial/img/tower.png");
   resourcePhone = anim.AddResource("/home/matrix/ns3/ns-allinone-3.28/ns-3.28/examples/tutorial/img/phone.png");
   //set some attributes
@@ -273,12 +296,14 @@ int main(int argc, char *argv[])
   anim.UpdateNodeColor(pgw, 255, 0, 0);
 
   //lym:sfz:update tower img
-  for(int i = 2; i < 2 + numberOfEnbs; i ++) {
+  for (int i = 2; i < 2 + numberOfEnbs; i++)
+  {
     anim.UpdateNodeImage(i, resourceTower);
     anim.UpdateNodeSize(i, 200, 200);
   }
   //lym:sfz:update phone img
-  for(int i = 2 + numberOfEnbs; i < 2 + numberOfEnbs + numberOfUes; i ++) {
+  for (int i = 2 + numberOfEnbs; i < 2 + numberOfEnbs + numberOfUes; i++)
+  {
     anim.UpdateNodeImage(i, resourcePhone);
     anim.UpdateNodeSize(i, 100, 100);
   }
